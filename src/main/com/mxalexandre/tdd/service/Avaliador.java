d3649c9bb887424745181181673491045228d596
Alexandre de Matos Xavier <alexandrexavier@inf.ufg.br>
2018-10-03 07:49:12 -0300

    Adding files

---
+++ src/main/com/mxalexandre/tdd/service/Avaliador.java
@@ -0,0 +1,26 @@
+package com.mxalexandre.tdd.service;
+
+import com.mxalexandre.tdd.domain.Lance;
+import com.mxalexandre.tdd.domain.Leilao;
+
+public class Avaliador {
+
+  private double maiorDeTodos = Double.NEGATIVE_INFINITY;
+  private double menorDeTodos = Double.POSITIVE_INFINITY;
+
+  public void avalia(Leilao leilao) {
+    for(Lance lance : leilao.getLances()) {
+      if(lance.getValor() > maiorDeTodos) maiorDeTodos = lance.getValor();
+      if(lance.getValor() < menorDeTodos) menorDeTodos = lance.getValor();
+    }
+  }
+
+  public double getMaiorLance() {
+    return maiorDeTodos;
+  }
+
+
+  public double getMenorLance() {
+    return menorDeTodos;
+  }
+}
---
+++ src/test/com/mxalexandre/tdd/service/TesteDoAvaliador.java
@@ -0,0 +1,33 @@
+package com.mxalexandre.tdd.service;
+
+import com.mxalexandre.tdd.domain.Lance;
+import com.mxalexandre.tdd.domain.Leilao;
+import com.mxalexandre.tdd.domain.Usuario;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TesteDoAvaliador {
+
+  @Test
+  public void main() {
+    Usuario joao = new Usuario("João");
+    Usuario jose = new Usuario("José");
+    Usuario maria = new Usuario("maria");
+
+    Leilao leilao = new Leilao(("Playsstation 3 novo"));
+
+    leilao.propoe(new Lance(joao, 300.0));
+    leilao.propoe(new Lance(jose, 400.0));
+    leilao.propoe(new Lance(maria, 250));
+
+    Avaliador leiloeiro = new Avaliador();
+    leiloeiro.avalia(leilao);
+
+    double maiorEsperado = 400;
+    double menorEsperado = 250;
+
+    Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+    Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+  }
+
+}
